81beaa72dfd605beb593b3ce03edecf9086472d1
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-07-23 13:11:43 +0800

    Update pyttsx3test.py

--- pyttsx3test.py
+++ pyttsx3test.py
@@ -19,13 +19,13 @@ def text_to_speech(text):
     engine.runAndWait()
     with open("response.mp3", "rb") as file:
         response_audio = file.read()
-    os.remove("response.mp3")  # Remove the temporary audio file
+#    os.remove("response.mp3")  # Remove the temporary audio file
     return response_audio
 
 # Main function to run the Streamlit app
 def main():
     st.title("Audio to Chat App")
-    text = "Click the Record Button below and speak to the AI"
+    text = "中华人民共和国"
         # Audio output section
     st.header("Step 2: Listen to the AI Response")
     st.audio(text_to_speech(text), format="audio/mp3", start_time=0)
